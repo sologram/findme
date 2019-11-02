f532ebf0b638b79324f35ac5a7566ce11d8ef6b2
sologram <sologram@gmail.com>
2019-11-03 02:03:42 +0800

    led

--- findme.c
+++ findme.c
@@ -9,45 +9,76 @@
 
 #define MATCH0				(0x800u)
 #define MATCH1				(0x8000u)
-#define LED_TOGGLE_TIMEOUT	(100u)
 
-uint8	alert = 0;
+#define	MODE_NONE			0
+#define	MODE_ADVERTISING	1
+#define	MODE_CONNECTED		2
+#define	MODE_ALERT			3
+
+#define	TIMESET(x)	((int32)millisecond == (int32)-(x) ? 1 : millisecond + (x))
+#define	TIMEOUT(x)	((x) && (int32)(millisecond - (x)) > 0)
+
 uint8	beatcounter;
 uint32	millisecond;
+uint8	mode = 0;
+uint32	tmoLed;
 
 static CY_ISR(Beat) {
 	CySysWdtClearInterrupt(CY_SYS_WDT_COUNTER0_INT);
 	beatcounter++;
+	switch (mode) {
+	case MODE_ADVERTISING:
+		if (!(beatcounter & 31)) {
+			tmoLed = TIMESET(8);
+			LED_Write(LED_ON);
+		}
+		break;
+	case MODE_CONNECTED:
+		LED_Write(LED_ON);
+		break;
+	case MODE_ALERT:
+		if (!(beatcounter & 3)) {
+			tmoLed = TIMESET(20);
+			LED_Write(LED_ON);
+		}
+		break;
+	}
 }
 
 static CY_ISR(Mill) {
 	millisecond++;
+	if (TIMEOUT(tmoLed)) {
+		tmoLed = 0;
+		LED_Write(LED_OFF);
+	}
 }
 
 static void IasEvent(uint32 event, void *param) {
-	if (event == CYBLE_EVT_IASS_WRITE_CHAR_CMD)
-		CyBle_IassGetCharacteristicValue(CYBLE_IAS_ALERT_LEVEL, sizeof alert, &alert);
+	if (event == CYBLE_EVT_IASS_WRITE_CHAR_CMD) {
+		uint8 a;
+		CyBle_IassGetCharacteristicValue(CYBLE_IAS_ALERT_LEVEL, sizeof a, &a);
+		mode = a ? MODE_ALERT : MODE_CONNECTED;
+	}
 }
 
 static void StackEvent(uint32 event, void *param) {
 	switch(event) {
 	case CYBLE_EVT_STACK_ON:
 	case CYBLE_EVT_GAP_DEVICE_DISCONNECTED:
+		LED_Write(LED_OFF);
 		CyBle_GappStartAdvertisement(CYBLE_ADVERTISING_FAST);
-		Advertising_LED_Write(LED_ON);
-		alert = NO_ALERT;
+		mode = MODE_ADVERTISING;
 		break;
 	case CYBLE_EVT_GAP_DEVICE_CONNECTED:
-		Advertising_LED_Write(LED_OFF);
-		LED_Write(LED_OFF);
+		mode = MODE_CONNECTED;
 		break;
 	case CYBLE_EVT_GAPP_ADVERTISEMENT_START_STOP:
-		if (CyBle_GetState() == CYBLE_STATE_DISCONNECTED) {
-			Advertising_LED_Write(LED_OFF);
-			LED_Write(LED_ON);
-			CySysPmSetWakeupPolarity(CY_PM_STOP_WAKEUP_ACTIVE_HIGH);
-			CySysPmStop();
-		}
+//		if (CyBle_GetState() == CYBLE_STATE_DISCONNECTED) {
+//			Advertising_LED_Write(LED_OFF);
+//			LED_Write(LED_ON);
+//			CySysPmSetWakeupPolarity(CY_PM_STOP_WAKEUP_ACTIVE_HIGH);
+//			CySysPmStop();
+//		}
 		break;
 	}
 }
@@ -73,24 +104,8 @@ int main() {
 	CyBle_IasRegisterAttrCallback(IasEvent);
 
 	for (;;) {
-		static uint8 toggleTimeout = 0;
 		CyBle_ProcessEvents();
 		CySysWatchdogFeed(CY_SYS_WDT_COUNTER1);
-		switch(alert) {
-		case NO_ALERT:
-			Alert_LED_Write(LED_OFF);
-			break;
-		case MILD_ALERT:
-			toggleTimeout++;
-			if (toggleTimeout == LED_TOGGLE_TIMEOUT) {
-				Alert_LED_Write(Alert_LED_Read() ^ 0x01);
-				toggleTimeout = 0;
-			}
-			break;
-		case HIGH_ALERT:
-			Alert_LED_Write(LED_ON);
-			break;
-		}
 //		CYBLE_BLESS_STATE_T blessState;
 //		uint8 intrStatus;
 //		CyBle_EnterLPM(CYBLE_BLESS_DEEPSLEEP);
